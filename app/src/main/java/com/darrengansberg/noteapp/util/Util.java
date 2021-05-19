8f16c825b7f3874ae1c50356a5da1a2ad41e1136
Darren Gansberg <darren.gansberg@hotmail.com>
2021-05-19 11:21:49 +1000

    Add Utility class for note persistence layer.
    This change introduces a class that defines constants that are utilised in the persistence layer of the notes app including definition of:
    - DATABASE_NAME
    - DATABASE_VERSION
    - NOTES_TABLE_NAME
    - Column names for the Notes Table (NOTE_ID and NOTE_CONTENT)

---
+++ app/src/main/java/com/darrengansberg/noteapp/util/Util.java
@@ -0,0 +1,12 @@
+package com.darrengansberg.noteapp.util;
+
+public class Util {
+
+    public static int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "note_db";
+    public static final String NOTES_TABLE_NAME = "notes";
+
+    public static final String NOTE_ID = "note_id";
+    public static final String NOTE_CONTENT = "note_content";
+
+}
